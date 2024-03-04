6e867b61b581dce93e8253459918e696240c6389
SarahCallesDeveloper <sarahcallesdeveloper@gmail.com>
2024-03-04 19:59:55 -0300

    Basic Structure

---
+++ Board_Organizer/src/LayoutFacade/Main_LayoutFacade.jsx
@@ -0,0 +1,10 @@
+
+function Main_LayoutFacade(){
+    return(
+        <>
+        <div> main</div>
+        </>
+    )
+}
+
+export default Main_LayoutFacade
---
+++ Board_Organizer/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Main_LayoutFacade from "./LayoutFacade/Main_LayoutFacade"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <>
+    <Main_LayoutFacade/>
+    </>
+  </React.StrictMode>,
+)
